9a32d533ebdb091c4a4a80f7be314d8878e80b3d
EnAyPi <phong180907@gmail.com>
2021-01-10 09:36:30 +0700

    Tech-guide

--- js/search.js
+++ js/search.js
@@ -1,10 +1,3 @@
-let div = document.getElementById("myDropdown");
-let a = div.getElementsByTagName("a");
-for (let j = 0; j < a.length; j++) {
-    a[j].style.display = "none";
-}
-
-
 function filterFunction() {
     let more = document.getElementsByClassName("more");
     let filter = sessionStorage.getItem("search").toUpperCase();
@@ -27,17 +20,36 @@ function filterFunction() {
     }
 }
 document.getElementById("searchInput").addEventListener("keyup", function (event) {
-    sessionStorage.setItem("search", document.getElementById("searchInput").value);
     event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementById("search-icon").click();
+    if (document.getElementById("searchInput").value != "") {
+        sessionStorage.setItem("search", document.getElementById("searchInput").value);
+        if (event.keyCode === 13) {
+            document.getElementById("search-icon").click();
+        }
     }
+
 });
 
 document.getElementById("search2").addEventListener("keyup", function (event) {
-    sessionStorage.setItem("search", document.getElementById("search2").value);
     event.preventDefault();
-    if (event.keyCode === 13) {
+    if (document.getElementById("search2").value != "") {
+        sessionStorage.setItem("search", document.getElementById("search2").value);
+        if (event.keyCode === 13) {
+            document.getElementById("search-icon").click();
+        }
+    }
+
+});
+
+function tosearch() {
+    if (document.getElementById("searchInput").value != "") {
+        sessionStorage.setItem("search", document.getElementById("searchInput").value);
         document.getElementById("search-icon").click();
     }
-});
+}
+function tosearch2() {
+    if (document.getElementById("search2").value != "") {
+        sessionStorage.setItem("search", document.getElementById("search2").value);
+        document.getElementById("search-icon").click();
+    }
+}
